343c4405630ae23fcd569d9390e573d2598edfc9
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2023-02-25 11:14:10 +0530

    mile to km

--- Mile to Km Converter/main.py
+++ Mile to Km Converter/main.py
@@ -1,18 +1,5 @@
 import tkinter
 window = tkinter.Tk()
-# window.title('My GUI Programe')
-# window.minsize(width=500, height=300)
-# window.config(padx=20, pady=20)
-# mylabel = tkinter.Label(text='Its a Label', font=('Arial', 24, 'bold'))
-# mylabel.grid(row=0, column=0)
-# def button_click():
-#     mylabel.config(text=blankSpace.get())
-# blankSpace = tkinter.Entry()
-# blankSpace.grid(row=2, column=3)
-# button = tkinter.Button(text='Submit', command=button_click)
-# button.grid(row=1, column=1)
-# newbuttom = tkinter.Button(text='New Button', command=button_click)
-# newbuttom.grid(row=0, column=2)
 window.title('Mile To KM')
 window.config(padx=30, pady=30)
 Mileinput = tkinter.Entry()
