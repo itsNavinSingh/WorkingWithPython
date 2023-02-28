ed0a2eba526c537061de3559606dc67853bb2771
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2023-02-28 13:31:48 +0530

    Add next button in quiz

--- Computer Quiz/main.py
+++ Computer Quiz/main.py
@@ -4,7 +4,7 @@ import time
 ScoreCard = 0
 THEME_COLOR = "#375362"
 ANS = True
-def QPallet():
+def q_pallet():
     global ANS
     qna = api.qna()
     canvas.itemconfig(Question, text=f"{qna['Question']}")
@@ -12,6 +12,7 @@ def QPallet():
         ANS = True
     else:
         ANS = False
+    canvas.configure(background='white')
 
 
 def click_right():
@@ -23,7 +24,6 @@ def click_right():
         canv_color = 'red'
     canvas.configure(background=canv_color)
     score.config(text=f'Score: {ScoreCard}')
-    QPallet()
 
 def click_wrong():
     canvas.configure(background="green")
@@ -35,24 +35,24 @@ def click_wrong():
         canv_color = 'red'
     canvas.configure(background=canv_color)
     score.config(text=f'Score: {ScoreCard}')
-    time.sleep(1)
-    QPallet()
 
 
 window = tkinter.Tk()
 window.title('Quizzler')
 window.config(padx=10, pady=10, bg=THEME_COLOR)
 score = tkinter.Label(text='Score: 0', bg=THEME_COLOR, fg='white', font=('Arial', 20, 'bold'))
-score.grid(row=0, column=0, columnspan=2)
+score.grid(row=0, column=0, columnspan=3)
 canvas = tkinter.Canvas(width=400, height=400)
 canvas.configure(background='white')
 Question = canvas.create_text(200, 200, text='Quesion', width=350, font=('Arial', 20, 'normal'), fill='black')
-canvas.grid(row=1, column=0, columnspan=2)
+canvas.grid(row=1, column=0, columnspan=3)
 true_img = tkinter.PhotoImage(file='images/true.png')
 false_img = tkinter.PhotoImage(file='images/false.png')
 right_button = tkinter.Button(image=true_img, highlightthickness=0, command=click_right)
-right_button.grid(row=3, column=1)
+right_button.grid(row=3, column=2)
 wrong_button = tkinter.Button(image=false_img, highlightthickness=0, command=click_wrong)
 wrong_button.grid(row=3, column=0)
-QPallet()
+next_button = tkinter.Button(text='Next', highlightthickness=0, font=('Arial', 20, 'bold'), command=q_pallet)
+next_button.grid(row=3, column=1)
+q_pallet()
 window.mainloop()
