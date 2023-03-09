352346bf5376f024e9b2a2818320c4b8ecd2b2f0
Navin Singh <50445729+RussianVirus@users.noreply.github.com>
2023-03-09 21:41:56 +0530

    Using Web Scraping

--- Top 100 Movies/main.py
+++ Top 100 Movies/main.py
@@ -3,12 +3,8 @@ import requests
 
 response = requests.get(url='https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2')
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
-article = soup.find_all(name='h3', class_='title')
-assending_list = []
-
-for i in range (len(article)-1, -1, -1):
-    name = article[i].string
-    assending_list.append(name)
+movies = soup.find_all(name='h3', class_='title')
+assending_list = [movie.getText() for movie in movies][::-1]
 
 with open('top_movie.txt', mode='a', encoding='utf-8') as txt100:
     for movie in assending_list:
